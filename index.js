efa89bfba6ad95e11f43dd7de16d0e59a71812c9
Iaroslava <floweryaroslava@mail.ru>
2024-06-19 15:55:41 +0800

    Добавлен код с использованием lodash; node_modules исключена из отслеживания

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Hello, Hexlet!");
+// По общепринятому соглашению, lodash импортируется под именем _
+import _ from 'lodash';
+
+// Пересечение массивов
+const intersectedArray = _.intersection([2, 1], [2, 3]);
+console.log('Intersection:', intersectedArray); // => [2]
+
+// capitalize делает первую букву заглавной
+const capitalizedString = _.capitalize('hello');
+console.log('Capitalized:', capitalizedString); // => Hello
+console.log(_.last(['one', 'two']));
